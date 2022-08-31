af00c24395004e026f285abff1be361a19e77567
Akash Borate <27akashborate@gmail.com>
2022-08-31 15:44:27 +0530

    complete

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express= require("express");
+const Connect= require('./src/config/config');
 
 const BlogController= require('./src/controllers/BlogController');
 const ReviewController= require('./src/controllers/ReviewController');
@@ -14,6 +15,6 @@ app.listen(7492, async()=>{
         await Connect();
         console.log("Listening on port 7492");
     } catch (error) {
-        console.log(error.massage);
+        console.log(error.messege);
     }
 });
