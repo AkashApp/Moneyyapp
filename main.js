98940384125d38b2ba725a292f39f9c8a2958d8b
Akash Borate <27akashborate@gmail.com>
2022-08-31 16:32:22 +0530

    project deployed

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const express= require("express");
+const cors= require("cors");
 const Connect= require('./src/config/config');
 
 const BlogController= require('./src/controllers/BlogController');
 const ReviewController= require('./src/controllers/ReviewController');
 
 const app= express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/blog', BlogController);
 app.use('/review', ReviewController);
 
-app.listen(7492, async()=>{
+app.listen(process.env.PORT || 7492, async()=>{
     try {
         await Connect();
         console.log("Listening on port 7492");
